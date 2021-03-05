0d2739b87c4f0c5ad9c9188570b15c33f61346be
venkatmallipeddi <80127906+venkatmallipeddi@users.noreply.github.com>
2021-03-05 15:57:07 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
- terraform is excellent 03052021
+ terraform is excellent 03052021. create a pull request
   <!-- END -->
 
   </div>
